8e507e5d29809fcddfe53dc37ae21ec988567404
Miranda Wang <mswang@etsy.com>
2020-06-20 16:06:46 -0400

    Setting up initial React files + include react router dom

---
+++ lets-eat-frontend/src/App.js
@@ -0,0 +1,34 @@
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+export default function App() {
+  return (
+    <Router>
+      <div>
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/login">
+            <LoginForm />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+function Home() {
+  return <h2>Home</h2>;
+}
+
+function LoginForm() {
+  return <h2>Login Form Placeholder</h2>;
+}
