32c82ca3d70a202847445351d98c3f6c229ea710
kev-kev <kevin.clark93@gmail.com>
2020-06-22 15:41:50 -0400

    add recipe submit

--- rails-backend/config/routes.rb
+++ rails-backend/config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   post "/signup", to: "users#signup"
   post "/login", to: "users#login"
+
+  resources :recipes, only: [:index]
+  post "/submit", to: "recipes#submit"
+  put '/recipes/:id', to: "recipes#change_status"
 end
