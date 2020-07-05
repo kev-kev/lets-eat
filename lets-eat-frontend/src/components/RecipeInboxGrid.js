340c502ce59ccb8964689752a29590cf4eb04b15
kev-kev <kevin.clark93@gmail.com>
2020-07-05 17:38:33 -0400

    Only rendering recipe vote card if you didnt submit it

--- lets-eat-frontend/src/components/RecipeInboxGrid.js
+++ lets-eat-frontend/src/components/RecipeInboxGrid.js
@@ -4,29 +4,31 @@ import RecipeCard from './RecipeCard';
 import { GlobalContext } from '../context/GlobalState';
 
 
-const renderRecipeCard = (recipe) => {
+const renderRecipeCard = (recipe, currentUser) => {
+  if (recipe.submittedBy !== currentUser.username) {
     return (
-        <Grid item xs={12} sm={6} md={3} xl={2} key={recipe.name} >
-          <RecipeCard
-            id={recipe.id}
-            isRecipeVoteCard={true}
-            name={recipe.name}
-            imgUrl={recipe.imgUrl}
-            link={recipe.link}
-            submittedBy={recipe.submittedBy} />
-        </Grid>
+      <Grid item xs={12} sm={6} md={3} xl={2} key={recipe.name} >
+        <RecipeCard
+          id={recipe.id}
+          isRecipeVoteCard={true}
+          name={recipe.name}
+          imgUrl={recipe.imgUrl}
+          link={recipe.link}
+          submittedBy={recipe.submittedBy} />
+      </Grid>
     );
+  }
 }
 
-const renderRecipeGrid = (recipes) => {
+const renderRecipeGrid = (recipes, currentUser) => {
   const pendingRecipes = recipes.filter(recipe => recipe.status === "pending")
     return pendingRecipes.map(recipe => {
-        return renderRecipeCard(recipe);
+        return renderRecipeCard(recipe, currentUser);
     });
 }
 
 export default function RecipeGrid() {
-  const { recipes } = useContext(GlobalContext)
+  const { recipes, user } = useContext(GlobalContext)
   return (
     <Grid 
       direction="row"
@@ -34,7 +36,7 @@ export default function RecipeGrid() {
       alignItems="flex-start"
       spacing={3}
       container>
-        {renderRecipeGrid(recipes)}
+        {renderRecipeGrid(recipes, user)}
     </Grid>
   );
 }
