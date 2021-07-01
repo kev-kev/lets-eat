d2019e5c82588b6208a9a5101e643275488d5308
kev-kev <kevin.clark93@gmail.com>
2021-07-01 18:42:58 -0400

    adjusting routes

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   post "/signup", to: "users#signup"
   post "/login", to: "users#login"
 
-  resources :recipes, only: [:index, :destroy, :update]
+  resources :recipes, only: [:index, :delete, :update]
   post "/submit", to: "recipes#submit"
 end
