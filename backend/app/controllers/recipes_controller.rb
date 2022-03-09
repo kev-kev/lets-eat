8bbdc91b3283127114e9eae3914a2324a5fbecbc
kev-kev <kevin.clark93@gmail.com>
2022-03-09 17:25:28 -0500

    improve recipe update to only delete necessary ingredients

--- backend/app/controllers/recipes_controller.rb
+++ backend/app/controllers/recipes_controller.rb
@@ -8,56 +8,38 @@ class RecipesController < ApplicationController
   end
 
   def submit
-    @recipe = Recipe.new(recipe_params.except(:ingredients))
-    @user = User.find(recipe_params[:user_id])
-    @recipe.user = @user
-    if @recipe.valid?
+    recipe = Recipe.new(recipe_params.except(:ingredients))
+    user = User.find(recipe_params[:user_id])
+    recipe.user = user
+    if recipe.valid?
       ingredients = recipe_params[:ingredients]
-      @recipe.save!
+      recipe.save!
       ingredients.each do |ingredient|
         new_ingredient = Ingredient.find_or_create_by({"name"=> ingredient[:name], "unit" => ingredient[:unit]})
         if new_ingredient.valid?
-          recipe_ingredient = RecipeIngredient.find_or_create_by({"recipe_id"=> @recipe[:id], "ingredient_id"=> new_ingredient[:id], "count"=>ingredient[:count]})
+          recipe_ingredient = RecipeIngredient.find_or_create_by({"recipe_id"=> recipe[:id], "ingredient_id"=> new_ingredient[:id], "count"=>ingredient[:count]})
         else
           render json: {error: 'uh oh! your ingredients are invalid 🥺👉👈'}, status: 400
         end
       end
-      render json: {recipe: @recipe}, status: 200
+      render json: {recipe: recipe}, status: 200
     else
       render json: {error: 'uh oh! your recipe is invalid 🥺👉👈'}, status: 400
     end
   end
 
   def update
-    # binding.pry
-    @recipe = Recipe.find(params[:id])
-
-    if @recipe
-
-#      @recipe.ingredients = nil
-      # Clear out all recipeingredients that have recipe id
-      old_recipe_ingredients = RecipeIngredient.where({"recipe_id" => @recipe[:id]}).destroy_all
-
-      recipe_params[:ingredients].each do |ingredient|
-        new_ingredient = Ingredient.find_or_create_by({"name"=> ingredient[:name], "unit" => ingredient[:unit]})
-        if new_ingredient.valid?
-          recipe_ingredient = RecipeIngredient.create({
-            "recipe_id"=> @recipe[:id],
-            "ingredient_id"=> new_ingredient[:id],
-            "count"=>new_ingredient[:count]
-          })
+    recipe = Recipe.find(params[:id])
+    if recipe
+        update_recipe_ingredients(recipe, recipe_params[:ingredients])
+        if recipe.update(recipe_params.except(:ingredients))
+          render status: 200
+        else
+          render json: {error: "unable to update recipe"}, status: 400
         end
-      end
-
-      if @recipe.update(recipe_params.except(:ingredients))
-        render status: 200
-      else
-        render json: {error: "unable to update recipe"}, status: 400
-      end
     else
       render json: {error: "unable to update recipe, recipe could not be found"}, status: 400
     end
-   # p @recipe.errors.full_messages
   end
 
   def destroy
@@ -98,4 +80,34 @@ class RecipesController < ApplicationController
         ).count
       }
     end
+
+    def update_recipe_ingredients(recipe, new_ingredients)
+      old_ingredients = RecipeIngredient.where({"recipe_id" => recipe[:id]})
+      remove_ingredients(new_ingredients, old_ingredients)
+      add_ingredients(new_ingredients, old_ingredients, recipe[:id])
+    end
+
+    def remove_ingredients(new_ingredients, old_ingredients)
+      old_ingredients.each do |old_ingredient|
+        unless new_ingredients.includes(old_ingredient)
+          old_ingredient.destroy!
+        end
+      end
+    end
+
+    def add_ingredients(new_ingredients, old_ingredients, recipe_id)
+      new_ingredients.each do |ingredient|
+        unless old_ingredients.includes(ingredient)
+          new_ingredient = Ingredient.find_or_create_by({"name"=> ingredient[:name], "unit" => ingredient[:unit]})
+          if new_ingredient.valid?
+            recipe_ingredient = RecipeIngredient.find_or_create_by({
+              "recipe_id"=> recipe_id,
+              "ingredient_id"=> new_ingredient[:id],
+              "count"=>new_ingredient[:count]
+            })
+          end
+        end
+      end
+    end
+    
 end
